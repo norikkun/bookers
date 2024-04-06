04a6d07af7cbd07505d4aeb1c0c10bfe089cac4f
norikkun <161901725+norikkun@users.noreply.github.com>
2024-04-06 15:12:47 +0000

    [update] レイアウト前完成版

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      flash[:notice] = "Book was successfully created."
+      flash[:notice] = "Book was successfully updated."
     redirect_to book_path(@book.id)
   else
     @books = Book.all
@@ -32,6 +32,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
   
